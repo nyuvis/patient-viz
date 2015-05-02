f7b33e6b18972ce4efefd9c7eb801c93ad768a91
Josua Krause <josua.krause@gmail.com>
2015-05-02 15:11:27 -0400

    using the correct code files

--- test/run.sh
+++ test/run.sh
@@ -13,15 +13,20 @@ format="../format.json"
 style_classes="../style_classes.json"
 etc="./etc"
 config="${etc}/config.txt"
+config_fe="${etc}/config_fe.txt"
+
+print() {
+  echo "$@" 2>&1
+}
 
 check() {
   if [ -s $ERR_FILE ]; then
     cat $ERR_FILE
-    echo "^ error file not empty ^"
+    print "^ error file not empty ^"
     exit 1
   fi
   if [ $1 -ne 0 ]; then
-    echo "^ command failed ^"
+    print "^ command failed ^"
     exit 2
   fi
 }
@@ -29,12 +34,13 @@ check() {
 check_file() {
   diff $1 $2
   if [ $? -ne 0 ]; then
-    echo "^ $1 doesn't match $2 ^"
+    print "^ $1 doesn't match $2 ^"
     exit 3
   fi
 }
 
 convert_patient() {
+  print "test: convert patient"
   rm -- "${OUTPUT}/${id}.json.tmp" 2> /dev/null
   rm -- "${OUTPUT}/dictionary.json.tmp" 2> /dev/null
   id=$1
@@ -47,21 +53,22 @@ convert_patient() {
 }
 
 create_predictive_model() {
+  print "test: create predictive model"
   rm -- "${OUTPUT}/cohort_cases.txt.tmp" 2> /dev/null
   rm -- "${OUTPUT}/cohort_control.txt.tmp" 2> /dev/null
   rm -- "${OUTPUT}/cohort.txt.tmp" 2> /dev/null
   rm -- "${OUTPUT}/output.csv.tmp" 2> /dev/null
   rm -- "${OUTPUT}/headers.json.tmp" 2> /dev/null
-  ${FEATURE_EXTRACT}/cohort.py --debug --query-file "${etc}/cases.txt" -f "${format}" -c "${config}" -o "${OUTPUT}/cohort_cases.txt.tmp" -- "${CMS_DIR}"
+  ${FEATURE_EXTRACT}/cohort.py --debug --query-file "${etc}/cases.txt" -f "${format}" -c "${config_fe}" -o "${OUTPUT}/cohort_cases.txt.tmp" -- "${CMS_DIR}"
   check $?
   check_file "${OUTPUT}/cohort_cases.txt" "${OUTPUT}/cohort_cases.txt.tmp"
-  ${FEATURE_EXTRACT}/cohort.py --debug --query-file "${etc}/control.txt" -f "${format}" -c "${config}" -o "${OUTPUT}/cohort_control.txt.tmp" -- "${CMS_DIR}"
+  ${FEATURE_EXTRACT}/cohort.py --debug --query-file "${etc}/control.txt" -f "${format}" -c "${config_fe}" -o "${OUTPUT}/cohort_control.txt.tmp" -- "${CMS_DIR}"
   check $?
   check_file "${OUTPUT}/cohort_control.txt" "${OUTPUT}/cohort_control.txt.tmp"
   ${FEATURE_EXTRACT}/merge.py --cases "${OUTPUT}/cohort_cases.txt.tmp" --control "${OUTPUT}/cohort_control.txt.tmp" -o "${OUTPUT}/cohort.txt.tmp" --test 30 --seed 0 2> $ERR_FILE
   check $?
   check_file "${OUTPUT}/cohort.txt" "${OUTPUT}/cohort.txt.tmp"
-  ${FEATURE_EXTRACT}/extract.py --debug -w "${OUTPUT}/cohort.txt.tmp" --num-cutoff 1 --age-time 20100101 --to 20100101 -o "${OUTPUT}/output.csv.tmp" -f "${format}" -c "${config}" -- "${CMS_DIR}"
+  ${FEATURE_EXTRACT}/extract.py --debug -w "${OUTPUT}/cohort.txt.tmp" --num-cutoff 1 --age-time 20100101 --to 20100101 -o "${OUTPUT}/output.csv.tmp" -f "${format}" -c "${config_fe}" -- "${CMS_DIR}"
   check $?
   check_file "${OUTPUT}/output.csv" "${OUTPUT}/output.csv.tmp"
   head -n 1 "${OUTPUT}/output.csv.tmp" | sed "s/,/ /g" | ../build_dictionary.py --debug -o "${OUTPUT}/headers.json.tmp" -c "${config}" --lookup -
